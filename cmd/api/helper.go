9d4fb4de39d1350fb314a737da90207d5e57d8e9
Mohit Singh Kanyal <kanyalmohit06@gmail.com>
2024-09-19 22:56:06 +0530

    Update helper.go
    updated readJSON so now when a post request comes it takes only 1 single JSON and will return if passed many json files

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -42,7 +43,14 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	//limiting size to 1mb
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	//Initializing the json.Decoder and calling the DisallowUnkownFields() method on it before decoding.
+	//if json from the client inculdes any field which can be mapped to target destination,the decoder will return an error instead of just ignoring the field
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -61,6 +69,15 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+
+		//we trim the preffix and put our format of error
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unkown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
 		case errors.As(err, &invalidUnmarshallError):
 			panic(err)
 
@@ -68,5 +85,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 			return err
 		}
 	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
